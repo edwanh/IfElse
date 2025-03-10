d61b066b3457806437d35f1c13a6c1c6fdab012c
Edwan <edwanhajjaj@icloud.com>
2025-03-10 12:16:02 +0100

    Taschenrechner

---
+++ main6.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+
+void main() {
+  while (true) {
+    print("Gib die erste Zahl ein: ");
+    double? number1 = double.tryParse(stdin.readLineSync()!);
+    if (number1 == null) {
+      print("Ungültig Bitte eine Zahl eingeben");
+      continue;
+    }
+
+    print("Gib ein Rechenzeichen (+, -, *, /) ein: ");
+    String? mathOperator = stdin.readLineSync();
+    if (mathOperator == null || !['+', '-', '*', '/'].contains(mathOperator)) {
+      print("Ungültiges Zeichen Bitte +, -, *, / eingeben.");
+      continue;
+    }
+
+    print("Gib die zweite Zahl ein: ");
+    double? number2 = double.tryParse(stdin.readLineSync()!);
+    if (number2 == null) {
+      print("Ungültig. Bitte eine Zahl eingeben.");
+      continue;
+    }
+
+    double solution;
+    switch (mathOperator) {
+      case '+':
+        solution = number1 + number2;
+        break;
+      case '-':
+        solution = number1 - number2;
+        break;
+      case '*':
+        solution = number1 * number2;
+        break;
+      case '/':
+        solution = number1 / number2;
+        break;
+      default:
+        print("Ungültig");
+        continue;
+    }
+
+    print("Ergebnis: $solution");
+  }
+}
