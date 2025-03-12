3277e827da90f9fe142bc1d39dbf16169a102deb
Edwan <edwanhajjaj@icloud.com>
2025-03-12 11:57:45 +0100

    revesre initials

--- main10.dart
+++ main10.dart
@@ -1,6 +1,7 @@
 void reverseInitials(String f, String l) => print(
   "${f[f.length - 1].toUpperCase()}. ${l[l.length - 1].toUpperCase()}.",
 );
+
 void main() {
   reverseInitials("Johnny", "Depp");
   reverseInitials("Tom", "Cruise");
